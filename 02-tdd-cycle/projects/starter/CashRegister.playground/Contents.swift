470755784154e6b02059feec0b6494d1ae1ec8c7
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-03-22 22:08:15 +0900

    2. The TDD Cycle

--- 02-tdd-cycle/projects/starter/CashRegister.playground/Contents.swift
+++ 02-tdd-cycle/projects/starter/CashRegister.playground/Contents.swift
@@ -33,15 +33,67 @@
 import XCTest
 
 class CashRegister {
-  
+    var availableFunds: Decimal
+    var transactionTotal: Decimal = 0
+    
+    init(availableFunds: Decimal) {
+        self.availableFunds = availableFunds
+    }
+    
+    func addItem(_ cost: Decimal) {
+        transactionTotal += cost
+    }
 }
 
 class CashRegisterTests: XCTestCase {
+    var availableFunds: Decimal!
+    var sut: CashRegister!
+    var itemCost: Decimal!
+    
     // 1
-    func testInit_createsCashRegister() {
-        // 2
-        XCTAssertNotNil(CashRegister())
+    override func setUp() {
+        super.setUp()
+        availableFunds = 100
+        itemCost = 42
+        sut = CashRegister(availableFunds: availableFunds)
     }
+    
+    // 2
+    override func tearDown() {
+        availableFunds = nil
+        itemCost = nil
+        sut = nil
+        super.tearDown()
+    }
+    
+    
+    func testInitAvailableFunds_setsAvailableFunds() {
+        // then
+        XCTAssertEqual(sut.availableFunds, availableFunds)
+    }
+    
+    func testAddItem_oneItem_addsCostToTransactionTotal() {
+        
+        // when
+        sut.addItem(itemCost)
+        
+        // then
+        XCTAssertEqual(sut.transactionTotal, itemCost)
+    }
+    
+    func testAddItem_twoItems_addsCostsToTransactionTotal() {
+        // given
+        let itemCost2 = Decimal(20)
+        let expectedTotal = itemCost + itemCost2
+        
+        // when
+        sut.addItem(itemCost)
+        sut.addItem(itemCost2)
+        
+        // then
+        XCTAssertEqual(sut.transactionTotal, expectedTotal)
+    }
+    
 }
 
 CashRegisterTests.defaultTestSuite.run()
